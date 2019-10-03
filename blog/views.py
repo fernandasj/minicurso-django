c95e8442f48c54e02cdf04c2ef811fed6597e015
fernandasj <fernanda.silva_sj@hotmail.com>
2019-10-03 17:48:35 -0300

    Adding Templates :tada:

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . import models
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = models.Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
